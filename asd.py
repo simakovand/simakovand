3e4e340998e4817140bc3eeca501fbadf36cd0b2
simakovand <82641842+simakovand@users.noreply.github.com>
2021-04-19 21:23:28 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -1,9 +1,13 @@
 count = 0
-while count <= 10:
-    on = input('Введите "а" для замыкания или размыкания цепи: ')
+#while count <= 10:
+while True:
+    on = input ('Введите "а" для замыкания или размыкания цепи, \n\
+А если хотите выйти, напишите "exit": ')
     if on == 'a' and count %2 == 0:
         print ('Лампочка выключена')
         count += 1
+    elif on == 'exit':
+        break
     else:
         print ('Лампочка включена')
         count +=1
