ae51a406751979c1061b88be0a8a96a1666fdc93
simakovand <82641842+simakovand@users.noreply.github.com>
2021-04-20 09:37:23 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -5,12 +5,10 @@ while True:
 А если хотите выйти, напишите "exit": ')
     if on == 'a' and a == True:
         print ('Лампочка включена')
-        if a == True:
-            a = False
+        a = False
 #        print(a)
-    elif on == 'a' and a == False:
-        if a == False:
-            a = True
+    elif on == 'a' and a == False:    
+        a = True
         print('Лампочка выключена')
 #        print(a)
     elif on == 'exit':
